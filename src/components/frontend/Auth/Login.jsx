314b45749994b97c811e18a9f1b8de6ba045e22a
Farhan Ahmed <farhanahmed6997@gmail.com>
2023-08-04 14:56:52 +0530

    date of birth done

--- src/components/frontend/Auth/Login.jsx
+++ src/components/frontend/Auth/Login.jsx
@@ -1,63 +1,51 @@
 import React from "react";
-function Login (){
-    return(
-        <>
-            <div className="rbt-elements-area bg-color-white rbt-section-gap">
-                <div className="container">
-                    <div className="row gy-5 row--30">
-                        <div className="col-lg-3"/>
-                        <div className="col-lg-6">
-                            <div className="rbt-contact-form contact-form-style-1 max-width-auto">
-                                <h3 className="title">Login</h3>
-                                <form className="max-width-auto" method="POST" action="">
-                                    <div className="form-group">
-                                        <input id="email" type="email"
-                                               name="email" required/>
-                                            <label>Email *</label>
-                                    </div>
-                                    <div className="form-group">
-                                        <input id="password" type="password"
-                                                name="password"
-                                               required/>
-                                            <label>Password *</label>
-                                            <span className="focus-border"/>
-                                    </div>
+import { Link } from "react-router-dom";
+import CustomInput from "../inputComponent/CustomInput";
+import Button from "../inputComponent/Button";
+function Login() {
+  return (
+    <>
+      <div className="rbt-elements-area bg-color-white rbt-section-gap">
+        <div className="container">
+          <div className="row gy-5 row--30">
+            <div className="col-lg-3" />
+            <div className="col-lg-6">
+              <div className="rbt-contact-form contact-form-style-1 max-width-auto">
+                <h3 className="title">Login</h3>
+                <form className="max-width-auto" method="POST" action="">
+                  <div className="form-group">
+                    <CustomInput type="email" Label="Email *" />
+                  </div>
+                  <div className="form-group">
+                    <CustomInput type="password" Label="Password *" />
+                  </div>
 
-                                    <div className="row mb--30">
-                                        <div className="col-lg-6">
-                                            <div className="rbt-checkbox">
-                                                <input type="checkbox" name="remember" id="remember"
-                                                    />
-                                                    <label htmlFor="remember">Remember me</label>
-                                            </div>
-                                        </div>
-                                        <div className="col-lg-6">
-                                            <div className="rbt-lost-password text-end">
-                                                <a className="rbt-btn-link" href="">
-                                                    Forgot Your Password
-                                                </a>
-                                            </div>
-                                        </div>
-                                    </div>
-
-                                    <div className="form-submit-group">
-                                        <button type="submit"
-                                                className="rbt-btn btn-md btn-gradient hover-icon-reverse w-100">
-                                    <span className="icon-reverse-wrapper">
-                                        <span className="btn-text">Log In</span>
-                                    <span className="btn-icon"><i className="feather-arrow-right"/></span>
-                                    <span className="btn-icon"><i className="feather-arrow-right"/></span>
-                                    </span>
-                                        </button>
-                                    </div>
-                                </form>
-                            </div>
-                        </div>
-                        <div className="col-lg-3"/>
+                  <div className="row mb--30">
+                    <div className="col-lg-6">
+                      <div className="rbt-checkbox">
+                        <input type="checkbox" name="remember" id="remember" />
+                        <label htmlFor="remember">Remember me</label>
+                      </div>
                     </div>
-                </div>
+                    <div className="col-lg-6">
+                      <div className="rbt-lost-password text-end">
+                        <Link className="rbt-btn-link" to="">
+                          Forgot Your Password
+                        </Link>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="form-submit-group">
+                    <Button name="Login" />
+                  </div>
+                </form>
+              </div>
             </div>
-        </>
-    )
+            <div className="col-lg-3" />
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
-export default Login
+export default Login;
--- src/components/frontend/Auth/Register.jsx
+++ src/components/frontend/Auth/Register.jsx
@@ -1,65 +1,70 @@
-import React from "react";
-function Register () {
-    return(
-        <>
-            <div className="rbt-elements-area bg-color-white rbt-section-gap">
-                <div className="container">
-                    <div className="row gy-5 row--30">
-                        <div className="col-lg-3"/>
-                        <div className="col-lg-6">
-                            <div className="rbt-contact-form contact-form-style-1 max-width-auto">
-                                <h3 className="title">Register</h3>
-                                <form className="max-width-auto" method="POST" action="">
-                                    <div className="form-group">
-                                        <input name="user_name"
-                                               type="text" required/>
-                                            <label>Username *</label>
-                                            <span className="focus-border"/>
+import CustomInput from "../inputComponent/CustomInput";
+import Button from "../inputComponent/Button";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import { useState, React } from "react";
 
-                                    </div>
-                                    <div className="form-group">
-                                        <input name="email"
-                                               type="email"
-                                               required
-                                        />
-                                        <label>Email address *</label>
-                                        <span className="focus-border"/>
-                                    </div>
-                                    <div className="form-group">
-                                        <input id="password" type="password"
-                                               className="password"
-                                               required />
+function Register() {
+  const [selectedDate, setSelectedDate] = useState(null);
+  return (
+    <>
+      <div className="rbt-elements-area bg-color-white rbt-section-gap">
+        <div className="container">
+          <div className="row gy-5 row--30">
+            <div className="col-lg-3" />
+            <div className="col-lg-6">
+              <div className="rbt-contact-form contact-form-style-1 max-width-auto">
+                <h3 className="title">Register</h3>
+                <form className="max-width-auto" method="POST" action="">
+                  <div className="form-group">
+                    <CustomInput type="text" Label="Full Name" required />
+                  </div>
+                  <div className="form-group">
+                    <CustomInput
+                      type="email"
+                      Label="Email address *"
+                      required
+                    />
+                  </div>
+                  <div className="form-group">
+                    <CustomInput type="password" Label="Password" required />
 
-                                            <label>Password *</label>
-                                            <span className="focus-border"/>
-                                    </div>
+                    <span className="focus-border" />
+                  </div>
 
-                                    <div className="form-group">
-                                        <input id="password-confirm" type="password" name="password_confirmation"
-                                               required/>
-                                            <label>Confirm Password *</label>
-                                            <span className="focus-border"/>
-                                    </div>
+                  <div className="form-group">
+                    <CustomInput
+                      type="password"
+                      Label="Confirm Password "
+                      required
+                    />
+                  </div>
 
-                                    <div className="form-submit-group">
-                                        <button type="submit"
-                                                className="rbt-btn btn-md btn-gradient hover-icon-reverse w-100">
-                                    <span className="icon-reverse-wrapper">
-                                        <span className="btn-text">Register</span>
-                                    <span className="btn-icon"><i className="feather-arrow-right"/></span>
-                                    <span className="btn-icon"><i className="feather-arrow-right"/></span>
-                                    </span>
-                                        </button>
-                                    </div>
+                  <div className="form-group">
+                    <DatePicker
+                      selected={selectedDate}
+                      onChange={(data) => setSelectedDate(data)}
+                      dataFormat="dd/MM/yyyy"
+                      filterDate={(date) =>
+                        date.getDay() != 6 && date.getDay() != 0
+                      }
+                      isClearable
+                      showYearDropdown
+                      scrollableMonthYearDropdown
+                    />
+                  </div>
 
-                                </form>
-                            </div>
-                        </div>
-                        <div className="col-lg-3"/>
-                    </div>
-                </div>
+                  <div className="form-submit-group">
+                    <Button name="Register" />
+                  </div>
+                </form>
+              </div>
             </div>
-        </>
-    )
+            <div className="col-lg-3" />
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
-export default Register
+export default Register;
