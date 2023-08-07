47c4f38a53457b5cf2a369d29af5ecdc5836ff60
samantaavijit <samantaavijit.2000@gmail.com>
2023-08-07 21:24:57 +0530

    registration complete

--- src/components/frontend/Auth/Login.jsx
+++ src/components/frontend/Auth/Login.jsx
@@ -1,63 +1,70 @@
-import React from "react";
-function Login (){
-    return(
-        <>
-            <div className="rbt-elements-area bg-color-white rbt-section-gap">
-                <div className="container">
-                    <div className="row gy-5 row--30">
-                        <div className="col-lg-3"/>
-                        <div className="col-lg-6">
-                            <div className="rbt-contact-form contact-form-style-1 max-width-auto">
-                                <h3 className="title">Login</h3>
-                                <form className="max-width-auto" method="POST" action="">
-                                    <div className="form-group">
-                                        <input id="email" type="email"
-                                               name="email" required/>
-                                            <label>Email *</label>
-                                    </div>
-                                    <div className="form-group">
-                                        <input id="password" type="password"
-                                                name="password"
-                                               required/>
-                                            <label>Password *</label>
-                                            <span className="focus-border"/>
-                                    </div>
+import React, { Component } from "react";
 
-                                    <div className="row mb--30">
-                                        <div className="col-lg-6">
-                                            <div className="rbt-checkbox">
-                                                <input type="checkbox" name="remember" id="remember"
-                                                    />
-                                                    <label htmlFor="remember">Remember me</label>
-                                            </div>
-                                        </div>
-                                        <div className="col-lg-6">
-                                            <div className="rbt-lost-password text-end">
-                                                <a className="rbt-btn-link" href="">
-                                                    Forgot Your Password
-                                                </a>
-                                            </div>
-                                        </div>
-                                    </div>
+export default class Login extends Component {
+  render() {
+    return (
+      <div className="rbt-elements-area bg-color-white rbt-section-gap">
+        <div className="container">
+          <div className="row gy-5 row--30">
+            <div className="col-lg-3" />
+            <div className="col-lg-6">
+              <div className="rbt-contact-form contact-form-style-1 max-width-auto">
+                <h3 className="title">Login</h3>
+                <form className="max-width-auto" method="POST" action="">
+                  <div className="form-group">
+                    <input id="email" type="email" name="email" required />
+                    <label>Email *</label>
+                  </div>
+                  <div className="form-group">
+                    <input
+                      id="password"
+                      type="password"
+                      name="password"
+                      required
+                    />
+                    <label>Password *</label>
+                    <span className="focus-border" />
+                  </div>
 
-                                    <div className="form-submit-group">
-                                        <button type="submit"
-                                                className="rbt-btn btn-md btn-gradient hover-icon-reverse w-100">
-                                    <span className="icon-reverse-wrapper">
-                                        <span className="btn-text">Log In</span>
-                                    <span className="btn-icon"><i className="feather-arrow-right"/></span>
-                                    <span className="btn-icon"><i className="feather-arrow-right"/></span>
-                                    </span>
-                                        </button>
-                                    </div>
-                                </form>
-                            </div>
-                        </div>
-                        <div className="col-lg-3"/>
+                  <div className="row mb--30">
+                    <div className="col-lg-6">
+                      <div className="rbt-checkbox">
+                        <input type="checkbox" name="remember" id="remember" />
+                        <label htmlFor="remember">Remember me</label>
+                      </div>
                     </div>
-                </div>
+                    <div className="col-lg-6">
+                      <div className="rbt-lost-password text-end">
+                        <a className="rbt-btn-link" href="">
+                          Forgot Your Password
+                        </a>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div className="form-submit-group">
+                    <button
+                      type="submit"
+                      className="rbt-btn btn-md btn-gradient hover-icon-reverse w-100"
+                    >
+                      <span className="icon-reverse-wrapper">
+                        <span className="btn-text">Log In</span>
+                        <span className="btn-icon">
+                          <i className="feather-arrow-right" />
+                        </span>
+                        <span className="btn-icon">
+                          <i className="feather-arrow-right" />
+                        </span>
+                      </span>
+                    </button>
+                  </div>
+                </form>
+              </div>
             </div>
-        </>
-    )
+            <div className="col-lg-3" />
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
-export default Login
--- src/components/frontend/Auth/Register.jsx
+++ src/components/frontend/Auth/Register.jsx
@@ -1,65 +1,249 @@
-import React from "react";
-function Register () {
-    return(
-        <>
-            <div className="rbt-elements-area bg-color-white rbt-section-gap">
-                <div className="container">
-                    <div className="row gy-5 row--30">
-                        <div className="col-lg-3"/>
-                        <div className="col-lg-6">
-                            <div className="rbt-contact-form contact-form-style-1 max-width-auto">
-                                <h3 className="title">Register</h3>
-                                <form className="max-width-auto" method="POST" action="">
-                                    <div className="form-group">
-                                        <input name="user_name"
-                                               type="text" required/>
-                                            <label>Username *</label>
-                                            <span className="focus-border"/>
+import React, { Component } from "react";
+import { Form } from "react-bootstrap";
+import validation from "../../../helper/MyValidation";
+import LoadingOverlay from "react-loading-overlay";
+import axios from "../../../helper/axios";
+import message from "../../../helper/message";
 
-                                    </div>
-                                    <div className="form-group">
-                                        <input name="email"
-                                               type="email"
-                                               required
-                                        />
-                                        <label>Email address *</label>
-                                        <span className="focus-border"/>
-                                    </div>
-                                    <div className="form-group">
-                                        <input id="password" type="password"
-                                               className="password"
-                                               required />
+export default class Register extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoading: false,
 
-                                            <label>Password *</label>
-                                            <span className="focus-border"/>
-                                    </div>
+      data: {
+        name: "",
+        email: "",
+        phone: "",
+        dob: "",
+        password: "",
+        c_password: "",
+      },
 
-                                    <div className="form-group">
-                                        <input id="password-confirm" type="password" name="password_confirmation"
-                                               required/>
-                                            <label>Confirm Password *</label>
-                                            <span className="focus-border"/>
-                                    </div>
+      errors: {},
+    };
+  }
 
-                                    <div className="form-submit-group">
-                                        <button type="submit"
-                                                className="rbt-btn btn-md btn-gradient hover-icon-reverse w-100">
-                                    <span className="icon-reverse-wrapper">
-                                        <span className="btn-text">Register</span>
-                                    <span className="btn-icon"><i className="feather-arrow-right"/></span>
-                                    <span className="btn-icon"><i className="feather-arrow-right"/></span>
-                                    </span>
-                                        </button>
-                                    </div>
+  handleChange = (e) => {
+    e.persist();
 
-                                </form>
-                            </div>
-                        </div>
-                        <div className="col-lg-3"/>
+    let { errors, data } = this.state;
+    let { name, value } = e.target;
+
+    data[name] = value;
+
+    this.setState({
+      data,
+    });
+
+    if (name === "c_password") {
+      if (data.password !== value) {
+        errors.c_password = "Confirm Password should be same as password";
+      } else {
+        errors.c_password = "";
+      }
+      this.setState({ errors });
+      return;
+    }
+
+    const valid_obj = {
+      value,
+      rules: e.target.getAttribute("validaterule"),
+      message: e.target.getAttribute("validatemsg"),
+    };
+
+    validation(valid_obj).then((err) => {
+      errors[name] = err;
+      this.setState({ errors });
+    });
+
+    if (name === "password") {
+      if (data.c_password && data.c_password !== value) {
+        errors.c_password = "Confirm Password should be same as password";
+      } else {
+        errors.c_password = "";
+      }
+      this.setState({ errors });
+    }
+  };
+
+  isValidForm = (errors) => {
+    let isValid = true;
+    for (const [, value] of Object.entries(errors)) {
+      if (value.length > 0) {
+        isValid = false;
+      }
+    }
+    return isValid;
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+
+    let { data, errors } = this.state;
+
+    if (this.isValidForm(errors)) {
+      this.setState({ isLoading: true });
+      axios
+        .post("/student-signup", data)
+        .then((res) => {
+          this.setState({ isLoading: false });
+
+          let { success, validation, error } = res.data;
+
+          if (validation) {
+            message.error(error[0].msg);
+          }
+
+          if (success) {
+            message.success(res.data.message);
+          } else {
+            message.error(res.data.message);
+          }
+        })
+        .catch((err) => {
+          this.setState({ isLoading: false });
+          message.error("Something Went Wrong");
+          console.error(err);
+        });
+    }
+  };
+
+  render() {
+    let { errors, data } = this.state;
+
+    return (
+      <LoadingOverlay active={this.state.isLoading} spinner text="Loading ...">
+        <div className="rbt-elements-area bg-color-white rbt-section-gap">
+          <div className="container">
+            <div className="row gy-5 row--30">
+              <div className="col-lg-3" />
+              <div className="col-lg-6">
+                <div className="rbt-contact-form contact-form-style-1 max-width-auto">
+                  <h3 className="title">Register</h3>
+                  <Form onSubmit={this.handleSubmit} className="max-width-auto">
+                    <div className="form-group">
+                      <input
+                        name="name"
+                        type="text"
+                        required
+                        validaterule={["required", "isName"]}
+                        validatemsg={["Last name is required"]}
+                        onChange={this.handleChange}
+                        value={data.name}
+                      />
+                      <label>Name *</label>
+                      <span className="focus-border" />
+                      <p className="invalid_input">{errors.name}</p>
                     </div>
+
+                    <div className="form-group">
+                      <input
+                        name="email"
+                        type="email"
+                        required
+                        validaterule={["required", "isEmail"]}
+                        validatemsg={[
+                          "Email address is required",
+                          "Enter a valid email address",
+                        ]}
+                        onChange={this.handleChange}
+                        value={data.email}
+                      />
+                      <label>Email *</label>
+                      <span className="focus-border" />
+                      <p className="invalid_input">{errors.email}</p>
+                    </div>
+
+                    <div className="form-group">
+                      <input
+                        name="phone"
+                        type="text"
+                        required
+                        validaterule={["required", "isMobile"]}
+                        validatemsg={[
+                          "Phone is required",
+                          "Enter a valid Phone Number",
+                        ]}
+                        onChange={this.handleChange}
+                        value={data.phone}
+                      />
+                      <label>Phone *</label>
+                      <span className="focus-border" />
+                      <p className="invalid_input">{errors.phone}</p>
+                    </div>
+
+                    <div className="form-group">
+                      <input
+                        name="dob"
+                        type="date"
+                        required
+                        validaterule={["required"]}
+                        validatemsg={["DOB is required"]}
+                        onChange={this.handleChange}
+                        value={data.dob}
+                      />
+                      <label>DOB *</label>
+                      <span className="focus-border" />
+                      <p className="invalid_input">{errors.dob}</p>
+                    </div>
+
+                    <div className="form-group">
+                      <input
+                        name="password"
+                        type="password"
+                        className="password"
+                        required
+                        validaterule={["required", "password"]}
+                        validatemsg={["Password is required"]}
+                        onChange={this.handleChange}
+                        value={data.password}
+                      />
+                      <label>Password *</label>
+                      <span className="focus-border" />
+                      <p className="invalid_input">{errors.password}</p>
+                    </div>
+
+                    <div className="form-group">
+                      <input
+                        name="c_password"
+                        type="password"
+                        className="password"
+                        required
+                        validaterule={["required"]}
+                        validatemsg={["Confirm Password is required"]}
+                        onChange={this.handleChange}
+                        value={data.c_password}
+                      />
+                      <label>Confirm Password *</label>
+                      <span className="focus-border" />
+                      <p className="invalid_input">{errors.c_password}</p>
+                    </div>
+
+                    <div className="form-submit-group">
+                      <button
+                        type="submit"
+                        className="rbt-btn btn-md btn-gradient hover-icon-reverse w-100"
+                      >
+                        <span className="icon-reverse-wrapper">
+                          <span className="btn-text">Register</span>
+                          <span className="btn-icon">
+                            <i className="feather-arrow-right" />
+                          </span>
+                          <span className="btn-icon">
+                            <i className="feather-arrow-right" />
+                          </span>
+                        </span>
+                      </button>
+                    </div>
+                  </Form>
                 </div>
+              </div>
+              <div className="col-lg-3" />
             </div>
-        </>
-    )
+          </div>
+        </div>
+      </LoadingOverlay>
+    );
+  }
 }
-export default Register
