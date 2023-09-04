6ffc998567621c37792d977c52b9812f7bcab0c0
samantaavijit <samantaavijit.2000@gmail.com>
2023-09-04 20:03:00 +0530

    courses complete

--- src/components/frontend/slider/CourseSliderCard.jsx
+++ src/components/frontend/slider/CourseSliderCard.jsx
@@ -1,114 +1,109 @@
-import React, {useState} from "react";
-import {Link} from "react-router-dom";
-import {BUCKET_DOMAIN} from "../../../helper/helper";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { BUCKET_DOMAIN } from "../../../helper/helper";
 import _ from "lodash";
 import axios from "../../../helper/axios";
 import config from "../../../helper/config";
 import message from "../../../helper/message";
 import { getCartDetails } from "../../../app/action/CartAction";
-import {useDispatch, useSelector} from "react-redux";
-export default function CourseSliderCard({ course,clasName}){
-    const [isLoading, setIsLoading] = useState(false);
-    const dispatch = useDispatch();
-    const cartDetails = useSelector((state) => state.cart_details);
+import { useDispatch, useSelector } from "react-redux";
 
-    const addToCart = (courseId) => {
-        setIsLoading(true);
+export default function CourseSliderCard({ course, clasName }) {
+  const [isLoading, setIsLoading] = useState(false);
+  const dispatch = useDispatch();
+  const cartDetails = useSelector((state) => state.cart_details);
 
-        const formData = {
-            course_id: courseId,
-        };
+  const addToCart = (courseId) => {
+    setIsLoading(true);
 
-        axios
-            .post("/add-to-cart", formData, config)
-            .then((res) => {
-                setIsLoading(false);
-                if (res.data.success) {
-                    message.success(res.data.message);
-                    dispatch(getCartDetails());
-                } else {
-                    message.error(res.data.message);
-                }
-            })
-            .catch((err) => {
-                setIsLoading(false);
-                message.error("Something Went Wrong!!!");
-                console.error(err);
-            });
+    const formData = {
+      course_id: courseId,
     };
 
-    const isPresent = (id, data = []) => {
-        return _.findIndex(data, (o) => o.course_id._id === id) !== -1;
-    };
+    axios
+      .post("/add-to-cart", formData, config)
+      .then((res) => {
+        setIsLoading(false);
+        if (res.data.success) {
+          message.success(res.data.message);
+          dispatch(getCartDetails());
+        } else {
+          message.error(res.data.message);
+        }
+      })
+      .catch((err) => {
+        setIsLoading(false);
+        message.error("Something Went Wrong!!!");
+        console.error(err);
+      });
+  };
 
-    const allCarts = !cartDetails.loading && cartDetails.cart_details ? cartDetails.cart_details.carts : [];
-    return(
-        <>
-            <div className={clasName}>
-                <div className="rbt-card variation-01 rbt-hover">
-                    <div className="rbt-card-img">
-                        <Link to={`/course-details/${course._id}`}>
-                            <img
-                                src={BUCKET_DOMAIN + course.thumbnail}
-                                alt={course.title}
-                            />
-                            {/*<div className="rbt-badge-3-custom bg-white">*/}
-                            {/*    <span>-50%</span>*/}
-                            {/*    <span>Off</span>*/}
-                            {/*</div>*/}
-                        </Link>
-                    </div>
-                    <div className="rbt-card-body">
-                        <ul className="rbt-meta">
-                            <li>
-                                <i className="feather-book" />
-                                12 Lessons
-                            </li>
-                            <li>
-                                <i className="feather-users" />
-                                50 Students
-                            </li>
-                        </ul>
-                        <h4 className="rbt-card-title">
-                            <Link to={`/course-details/${course._id}`}>
-                                {course.title}
-                            </Link>
-                        </h4>
-                        <p className="rbt-card-text">description</p>
-                        <div className="rbt-review">
-                            <div className="rating">
-                                <i className="fas fa-star"></i>
-                                <i className="fas fa-star"></i>
-                                <i className="fas fa-star"></i>
-                                <i className="fas fa-star"></i>
-                                <i className="fas fa-star"></i>
-                            </div>
-                            <span className="rating-count">
-                                (15 Reviews)
-                              </span>
-                        </div>
-                        <div className="rbt-card-bottom">
-                            <div className="rbt-price">
-                                <span className="current-price">${course.price}</span>
-                                <span className="off-price">$120</span>
-                            </div>
-                            {isPresent(course._id, allCarts) ? (
-                                <Link to="/cart" className="rbt-btn-link left-icon">
-                                    Go To Cart
-                                </Link>
-                            ) : (
-                                <Link
-                                    to="#"
-                                    className="rbt-btn-link left-icon"
-                                    onClick={() => addToCart(course._id)}
-                                >
-                                    <i className="feather-shopping-cart" /> Add To Cart
-                                </Link>
-                            )}
-                        </div>
-                    </div>
-                </div>
+  const isPresent = (id, data = []) => {
+    return _.findIndex(data, (o) => o.course_id._id === id) !== -1;
+  };
+
+  const allCarts =
+    !cartDetails.loading && cartDetails.cart_details
+      ? cartDetails.cart_details.carts
+      : [];
+  return (
+    <div className={clasName}>
+      <div className="rbt-card variation-01 rbt-hover">
+        <div className="rbt-card-img">
+          <Link to={`/course-details/${course._id}`}>
+            <img src={BUCKET_DOMAIN + course.thumbnail} alt={course.title} />
+            {/*<div className="rbt-badge-3-custom bg-white">*/}
+            {/*    <span>-50%</span>*/}
+            {/*    <span>Off</span>*/}
+            {/*</div>*/}
+          </Link>
+        </div>
+        <div className="rbt-card-body">
+          <ul className="rbt-meta">
+            <li>
+              <i className="feather-book" />
+              12 Lessons
+            </li>
+            <li>
+              <i className="feather-users" />
+              50 Students
+            </li>
+          </ul>
+          <h4 className="rbt-card-title">
+            <Link to={`/course-details/${course._id}`}>{course.title}</Link>
+          </h4>
+          <p className="rbt-card-text">description</p>
+          <div className="rbt-review">
+            <div className="rating">
+              <i className="fas fa-star"></i>
+              <i className="fas fa-star"></i>
+              <i className="fas fa-star"></i>
+              <i className="fas fa-star"></i>
+              <i className="fas fa-star"></i>
             </div>
-        </>
-    )
+            <span className="rating-count">(15 Reviews)</span>
+          </div>
+          <div className="rbt-card-bottom">
+            <div className="rbt-price">
+              <span className="current-price">${course.price}</span>
+              <span className="off-price">$120</span>
+            </div>
+            {isPresent(course._id, allCarts) ? (
+              <Link to="/cart" className="rbt-btn-link left-icon">
+                Go To Cart
+              </Link>
+            ) : (
+              <Link
+                to="#"
+                className="rbt-btn-link left-icon"
+                onClick={() => addToCart(course._id)}
+              >
+                <i className="feather-shopping-cart" /> Add To Cart
+              </Link>
+            )}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/components/frontend/slider/SwiperSlider.jsx
+++ src/components/frontend/slider/SwiperSlider.jsx
@@ -1,65 +1,50 @@
-import {Swiper, SwiperSlide} from "swiper/react";
-import React, {useEffect, useState} from "react";
-import { Autoplay, Pagination ,EffectCoverflow} from 'swiper/modules';
+import { Swiper, SwiperSlide } from "swiper/react";
+import React, { useEffect, useState } from "react";
+import { Autoplay, Pagination, EffectCoverflow } from "swiper/modules";
 import axios from "../../../helper/axios";
 import message from "../../../helper/message";
 import CourseSliderCard from "./CourseSliderCard";
+import { useSelector } from "react-redux";
 
-function SwiperSlider(){
-    const [isLoading, setIsLoading] = useState(false);
-    const [courses, setCourses] = useState([]);
-    console.log(courses)
-    useEffect(() => {
-        setIsLoading(true);
+function SwiperSlider() {
+  // const [courses, setCourses] = useState([]);
 
-        async function fetchCourses() {
-            try {
-                const response = await axios.get("/all-courses");
-                if (response.data.success) {
-                    setCourses(response.data.courses);
-                } else {
-                    message.error(response.data.message);
-                }
-            } catch (error) {
-                setIsLoading(false);
-                message.error("Something Went Wrong!!!");
-                console.error(error);
-            }
-        }
+  let { courses } = useSelector((state) => state.courses);
 
-        fetchCourses();
-    }, []);
-    return(
-        <>
-            <div className="banner-card pb--60 mb--50 swiper mySwiper rbt-dot-bottom-center banner-swiper-active">
-                <Swiper effect={'coverflow'}
-                        grabCursor={true}
-                        centeredSlides={true}
-                        slidesPerView={'auto'}
-                        modules={[Autoplay,EffectCoverflow, Pagination]}
-                        coverflowEffect={{
-                            rotate: 50,
-                            stretch: 0,
-                            depth: 100,
-                            modifier: 1,
-                            slideShadows: true,
-                        }}
-                        pagination={{
-                            clickable: true,
-                        }}
-                        autoplay={{
-                            delay: 2500,
-                            disableOnInteraction: false,
-                        }}  className='swiper-wrapper'  >
-                    {/*// <!-- Start Single Card  -->*/}
-                    {courses.map((course, index) => (
-                        <SwiperSlide>
-                            <CourseSliderCard key={index} course={course} />
-                        </SwiperSlide>
-                    ))}
-                </Swiper>
-            </div>
-        </>
-    )
+  courses = courses ? courses.courses : [];
+
+  return (
+    <div className="banner-card pb--60 mb--50 swiper mySwiper rbt-dot-bottom-center banner-swiper-active">
+      <Swiper
+        effect={"coverflow"}
+        grabCursor={true}
+        centeredSlides={true}
+        slidesPerView={"auto"}
+        modules={[Autoplay, EffectCoverflow, Pagination]}
+        coverflowEffect={{
+          rotate: 50,
+          stretch: 0,
+          depth: 100,
+          modifier: 1,
+          slideShadows: true,
+        }}
+        pagination={{
+          clickable: true,
+        }}
+        autoplay={{
+          delay: 2500,
+          disableOnInteraction: false,
+        }}
+        className="swiper-wrapper"
+      >
+        {/*// <!-- Start Single Card  -->*/}
+        {courses.map((course, index) => (
+          <SwiperSlide key={index}>
+            <CourseSliderCard key={index} course={course} />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
+  );
 }
-export default SwiperSlider
+export default SwiperSlider;
