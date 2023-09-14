3514cfa15b137dd996162f812f6231bca73c7fe9
samantaavijit <samantaavijit.2000@gmail.com>
2023-09-14 21:15:39 +0530

    paypal order compolete

--- src/components/frontend/cart/Pay.jsx
+++ src/components/frontend/cart/Pay.jsx
@@ -1,44 +1,17 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { PayPalButtons } from "@paypal/react-paypal-js";
-import { useSelector } from "react-redux";
 import axios from "../../../helper/axios";
 import config from "../../../helper/config";
 import message from "../../../helper/message";
+import { useDispatch } from "react-redux";
+import { getCartDetails } from "../../../app/action/CartAction";
 
 export default function Pay() {
-  const { cart_details } = useSelector((state) => {
-    return state;
-  });
-
-  const [totalPrice, setTotalPrice] = useState(0);
-  const [loading, setLoading] = useState(false);
-  const [orderID, setOrderID] = useState("");
-  const [cartCourses, setCartCourses] = useState([]);
-
-  useEffect(() => {
-    window.scrollTo(0, 0);
-
-    let data = cart_details.cart_details;
-    if (data) {
-      let { carts } = data;
-
-      let totalPrice = 0;
-
-      let cartCourses = carts.map(({ course_id }) => {
-        totalPrice += Number(course_id.price);
-        return course_id;
-      });
-
-      // setTotalPrice(totalPrice);
-      setCartCourses(cartCourses);
-      setTotalPrice(cartCourses[0].price);
-    }
-  }, [cart_details]);
-
+  const dispatch = useDispatch();
   const createOrder = async () => {
     let order_id = "";
     await axios
-      .post("/create-paypal-order", { price: totalPrice }, config)
+      .post("/create-paypal-order", {}, config)
       .then((res) => {
         let { id } = res.data;
         order_id = id;
@@ -46,7 +19,6 @@ export default function Pay() {
         return id;
       })
       .catch((err) => {
-        setLoading(false);
         message.error("Something Went Wrong!!!");
         console.error(err);
       });
@@ -57,10 +29,12 @@ export default function Pay() {
     await axios
       .post(`/capture-paypal-orders/${data.orderID}`, {}, config)
       .then((res) => {
-        console.log("res", res);
+        if (res.data.success) {
+          message.success(res.data.message);
+          dispatch(getCartDetails());
+        }
       })
       .catch((err) => {
-        setLoading(false);
         message.error("Something Went Wrong!!!");
         console.error(err);
       });
@@ -69,8 +43,8 @@ export default function Pay() {
   return (
     <PayPalButtons
       style={{ layout: "vertical" }}
-      createOrder={(data, actions) => createOrder(data)}
-      onApprove={(data, actions) => onApprove(data)}
+      createOrder={(data) => createOrder(data)}
+      onApprove={(data) => onApprove(data)}
     />
   );
 }
